946191427fa7cdec54a6ae720795daa4e7740116
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2022-09-12 01:09:47 -0400

    Created Dictionary type

--- src/lib.rs
+++ src/lib.rs
@@ -1,80 +1,94 @@
-use fst::raw::{Fst, Node};
+use fst::raw::Fst;
 use unicode_normalization::UnicodeNormalization;
 use unicode_segmentation::UnicodeSegmentation;
 
-pub fn load_dictionary<T>(words: &[T]) -> fst::Result<Fst<Vec<u8>>>
-where
-    T: AsRef<str>,
-{
-    let mut words = words
-        .iter()
-        .map(|word| word.as_ref())
-        .filter(|word| !word.is_empty())
-        .map(|word| word.chars().nfd().collect::<String>())
-        .collect::<Vec<_>>();
-    words.sort_unstable_by(|word1, word2| word1.as_bytes().cmp(word2.as_bytes()));
-    words.dedup();
-
-    Fst::from_iter_set(words.iter())
+#[derive(Clone)]
+#[repr(transparent)]
+pub struct Dictionary<D> {
+    fst: Fst<D>,
 }
 
-pub fn break_into_words<D>(word: &str, dictionary: &Fst<D>) -> Vec<Vec<String>>
+impl<'a> Dictionary<Vec<u8>> {
+    pub fn from_iter<I>(words: I) -> fst::Result<Self>
+    where
+        I: Iterator<Item = &'a str>,
+    {
+        let mut words = words
+            .filter(|word| !word.is_empty())
+            .map(|word| word.chars().nfd().collect::<String>())
+            .collect::<Vec<_>>();
+        words.sort_unstable_by(|word1, word2| word1.as_bytes().cmp(word2.as_bytes()));
+        words.dedup();
+
+        Fst::from_iter_set(words.into_iter()).map(|fst| Dictionary { fst })
+    }
+}
+
+impl Dictionary<Vec<u8>> {
+    pub fn new<T>(words: &[T]) -> fst::Result<Self>
+    where
+        T: AsRef<str>,
+    {
+        Self::from_iter(words.iter().map(|word| word.as_ref()))
+    }
+}
+
+impl<D> Dictionary<D>
 where
     D: AsRef<[u8]>,
 {
-    fn _break_into_words<D, S>(
-        results: &mut Vec<Vec<String>>,
-        prefix: &mut Vec<String>,
-        graphemes: &[S],
-        dictionary: &Fst<D>,
-        root: Node<'_>,
-    ) where
-        D: AsRef<[u8]>,
-        S: AsRef<str>,
-    {
-        prefix.push(String::new());
-        let mut current_node = root;
-
-        'outer: for (i, grapheme) in graphemes
-            .iter()
-            .map(|grapheme| grapheme.as_ref())
-            .enumerate()
+    pub fn concatenations_for(&self, word: &str) -> Vec<Vec<String>> {
+        fn _concatenations_for<D, S>(
+            dictionary: &Fst<D>,
+            graphemes: &[S],
+            prefix: &mut Vec<String>,
+            results: &mut Vec<Vec<String>>,
+        ) where
+            D: AsRef<[u8]>,
+            S: AsRef<str>,
         {
-            prefix.last_mut().unwrap().push_str(grapheme);
+            prefix.push(String::new());
+            let mut current_node = dictionary.root();
 
-            for byte in grapheme.bytes() {
-                let transition_index = match current_node.find_input(byte) {
-                    Some(index) => index,
-                    None => break 'outer,
-                };
-                current_node = dictionary.node(current_node.transition(transition_index).addr);
-            }
+            'outer: for (i, grapheme) in graphemes
+                .iter()
+                .map(|grapheme| grapheme.as_ref())
+                .enumerate()
+            {
+                prefix.last_mut().unwrap().push_str(grapheme);
 
-            if current_node.is_final() {
-                if i == graphemes.len() - 1 {
-                    results.push(prefix.clone());
-                } else {
-                    _break_into_words(results, prefix, &graphemes[i + 1..], dictionary, root);
+                for byte in grapheme.bytes() {
+                    let transition_index = match current_node.find_input(byte) {
+                        Some(index) => index,
+                        None => break 'outer,
+                    };
+                    current_node = dictionary.node(current_node.transition(transition_index).addr);
+                }
+
+                if current_node.is_final() {
+                    if i == graphemes.len() - 1 {
+                        results.push(prefix.clone());
+                    } else {
+                        _concatenations_for(dictionary, &graphemes[i + 1..], prefix, results);
+                    }
                 }
             }
+
+            prefix.pop();
         }
 
-        prefix.pop();
+        if word.is_empty() {
+            return vec![Vec::new()];
+        }
+
+        let word = word.chars().nfd().collect::<String>();
+        let graphemes = word.graphemes(true).collect::<Vec<_>>();
+        let mut results = Vec::new();
+
+        _concatenations_for(&self.fst, &graphemes, &mut Vec::new(), &mut results);
+
+        results
     }
-
-    if word.is_empty() {
-        return vec![Vec::new()];
-    }
-
-    let mut results = Vec::new();
-    let mut prefix = Vec::new();
-    let word = word.chars().nfd().collect::<String>();
-    let graphemes = word.graphemes(true).collect::<Vec<_>>();
-    let root = dictionary.root();
-
-    _break_into_words(&mut results, &mut prefix, &graphemes, dictionary, root);
-
-    results
 }
 
 #[cfg(test)]
@@ -83,16 +97,16 @@ mod tests {
 
     #[test]
     fn test_1() {
-        let dictionary = load_dictionary(&["b"]).unwrap();
-        let ways_to_concatenate = break_into_words("a", &dictionary);
+        let dictionary = Dictionary::new(&["b"]).unwrap();
+        let ways_to_concatenate = dictionary.concatenations_for("a");
 
         assert!(ways_to_concatenate.is_empty());
     }
 
     #[test]
     fn test_2() {
-        let dictionary = load_dictionary(&["b"]).unwrap();
-        let ways_to_concatenate = break_into_words("", &dictionary);
+        let dictionary = Dictionary::new(&["b"]).unwrap();
+        let ways_to_concatenate = dictionary.concatenations_for("");
 
         assert!(ways_to_concatenate.len() == 1);
         assert!(ways_to_concatenate[0].is_empty());
@@ -100,8 +114,8 @@ mod tests {
 
     #[test]
     fn test_3() {
-        let dictionary = load_dictionary(&["ab", "abc", "cd", "def", "abcd", "ef", "c"]).unwrap();
-        let mut ways_to_concatenate = break_into_words("abcdef", &dictionary);
+        let dictionary = Dictionary::new(&["ab", "abc", "cd", "def", "abcd", "ef", "c"]).unwrap();
+        let mut ways_to_concatenate = dictionary.concatenations_for("abcdef");
         ways_to_concatenate.sort_unstable();
 
         assert_eq!(
