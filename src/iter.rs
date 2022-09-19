4f8f6d54efc3ddb1ff42ed92e4ef31cf12a190cf
Chai T. Rex <ChaiTRex@users.noreply.github.com>
2022-09-19 14:47:56 -0400

    Sped up `.nth(n)`

--- src/iter.rs
+++ src/iter.rs
@@ -317,6 +317,152 @@ where
         None
     }
 
+    fn nth(&mut self, mut n: usize) -> Option<Self::Item> {
+        // While there are still available stack frames:
+        'outer: while let Some(mut stack_frame) = self.stack.pop() {
+            // If we're at the end of the input:
+            if stack_frame.grapheme_index == self.grapheme_bounds.len() {
+                // If the input itself wasn't empty:
+                if !self.input.is_empty() {
+                    // Backtrack.
+                    self.input_prefix_indexes.pop();
+                // If the input itself was empty:
+                } else {
+                    // We're done, so we deallocate the stack (the only thing allocated
+                    // when the input is empty) in case the programmer keeps this
+                    // iterator around.
+                    self.stack = Vec::new();
+
+                    // Produce an empty Vec for an empty input.
+                    return if n == 0 { Some(Vec::new()) } else { None };
+                }
+            // If we're not at the end of the input:
+            } else {
+                // Get the current input grapheme's end and the current NFD grapheme's
+                // start and end.
+                let input_nfd_grapheme_start_index = stack_frame
+                    .grapheme_index
+                    .checked_sub(1)
+                    .map(|i| unsafe { self.grapheme_bounds.get_unchecked(i) }.input_nfd_index)
+                    .unwrap_or(0);
+                let GraphemeBounds {
+                    input_index: input_grapheme_end_index,
+                    input_nfd_index: input_nfd_grapheme_end_index,
+                } = *unsafe {
+                    self.grapheme_bounds
+                        .get_unchecked(stack_frame.grapheme_index)
+                };
+
+                // For each byte in the current NFD grapheme:
+                for byte in self.input_nfd
+                    [input_nfd_grapheme_start_index..input_nfd_grapheme_end_index]
+                    .bytes()
+                {
+                    // If there are words in the Dictionary that also have this byte of
+                    // the NFD grapheme:
+                    if let Some(transition_index) = stack_frame.current_node.find_input(byte) {
+                        // Follow the path that includes this byte of the NFD grapheme
+                        // as well.
+                        stack_frame.current_node = self
+                            .dictionary
+                            .node(stack_frame.current_node.transition(transition_index).addr);
+                    // If there are no words in the Dictionary that also have this byte
+                    // of the NFD grapheme and there is more backtracking left to be
+                    // done:
+                    } else if !self.stack.is_empty() {
+                        // Backtrack.
+                        self.input_prefix_indexes.pop();
+                        continue 'outer;
+                    // If there are no words in the Dictionary that also have this byte
+                    // of the NFD grapheme and there is no backtracking left to be done:
+                    } else {
+                        // We're done, so we deallocate everything allocated in case
+                        // the programmer keeps this iterator around.
+                        self.input_nfd = String::new();
+                        self.grapheme_bounds = Vec::new();
+                        self.stack = Vec::new();
+                        self.input_prefix_indexes = Vec::new();
+                        return None;
+                    }
+                }
+
+                // Adjust the end of this part of the prefix to the end of the current
+                // NFD grapheme.
+                *self.input_prefix_indexes.last_mut().unwrap() = input_grapheme_end_index;
+
+                // If, after the current NFD grapheme is followed in the Dictionary, no
+                // words end here:
+                if !stack_frame.current_node.is_final() {
+                    // Continue with the next NFD grapheme.
+                    stack_frame.grapheme_index = stack_frame.grapheme_index.wrapping_add(1);
+                    self.stack.push(stack_frame);
+                // If, after the current grapheme is followed in the Dictionary, a word
+                // ends here and there are NFD graphemes left:
+                } else if input_nfd_grapheme_end_index < self.input_nfd.len() {
+                    // Recurse.
+                    self.input_prefix_indexes.push(input_grapheme_end_index);
+
+                    let grapheme_index = stack_frame.grapheme_index.wrapping_add(1);
+                    stack_frame.grapheme_index = grapheme_index;
+                    self.stack.push(stack_frame);
+                    self.stack.push(StackFrame {
+                        grapheme_index,
+                        current_node: self.dictionary.root(),
+                    });
+                // If, after the current grapheme is followed in the Dictionary, a word
+                // ends here, there are no NFD graphemes left, and we've reached the nth
+                // element:
+                } else if n == 0 {
+                    let input_prefix_indexes_iter = self.input_prefix_indexes.iter().copied();
+                    let result = core::iter::once(0)
+                        .chain(input_prefix_indexes_iter.clone())
+                        .zip(input_prefix_indexes_iter)
+                        .map(|(start, end)| &self.input[start..end])
+                        .collect::<Vec<_>>();
+
+                    // If there is some backtracking left to be done:
+                    if !self.stack.is_empty() {
+                        // Backtrack.
+                        self.input_prefix_indexes.pop();
+                    // If there's no remaining backtracking left to be done:
+                    } else {
+                        // We're done, so we deallocate everything allocated in case
+                        // the programmer keeps this iterator around.
+                        self.input_nfd = String::new();
+                        self.grapheme_bounds = Vec::new();
+                        self.stack = Vec::new();
+                        self.input_prefix_indexes = Vec::new();
+                    }
+
+                    return Some(result);
+                // If, after the current grapheme is followed in the Dictionary, a word
+                // ends here, there are no NFD graphemes left, we haven't reached the
+                // nth element, and there is some backtracking left to be done:
+                } else if !self.stack.is_empty() {
+                    n = n.wrapping_sub(1);
+
+                    // Backtrack.
+                    self.input_prefix_indexes.pop();
+                // If, after the current grapheme is followed in the Dictionary, a word
+                // ends here, there are no NFD graphemes left, we haven't reached the
+                // nth element, and there's no backtracking left to be done:
+                } else {
+                    // We're done, so we deallocate everything allocated in case
+                    // the programmer keeps this iterator around.
+                    self.input_nfd = String::new();
+                    self.grapheme_bounds = Vec::new();
+                    self.stack = Vec::new();
+                    self.input_prefix_indexes = Vec::new();
+
+                    return None;
+                }
+            }
+        }
+
+        // The work has all been done, so there are no more concatenations to produce.
+        None
+    }
+
     /*
     /* Nightly
     fn is_sorted(self) -> bool {
